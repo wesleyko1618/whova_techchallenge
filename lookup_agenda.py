b09370be8c8d23c8c8a5aa6ebd9775440a40d81d
wesleyko1618 <wesko887@gmail.com>
2022-09-16 21:14:25 -0700

    added subsession checking, looking through all speakers, and general error checking of inputs

--- lookup_agenda.py
+++ lookup_agenda.py
@@ -3,6 +3,13 @@ import sqlite3
 import sys
 from db_table import db_table
 
+#checking for subsessions when entering the session id number
+def checksub(num):
+    num += 1
+    while agenda.select(where={'id':num})[0]['session'] == 'Sub':
+        print(agenda.select(where={'id':num})[0])
+        num += 1
+
 #create a SQLite datatable with db_table with this schema
 agenda = db_table("agenda", { 
     "id": "integer PRIMARY KEY", 
@@ -17,14 +24,37 @@ agenda = db_table("agenda", {
 
 listofcol = ['date', 'time_start', 'time_end', 'title', 'location', 'description', 'speaker']
 
-col = sys.argv[1]
-val = sys.argv[2]
-
-if col not in listofcol:
+if (len(sys.argv) != 3):
+    print("Please only enter a colunn title and a value to search for")
     exit()
 
-info = agenda.select(where = {col:val})
-for x in info:
-    print(x)
+# inputs of column title and the value we are searching for
+#col = sys.argv[1]
+#val = sys.argv[2]
+
+#testing without sys.argv
+col = "date"
+val = "6/16/2018" 
+
+if col not in listofcol:
+    print("Please give a valid column title: 'date', 'time_start', 'time_end', 'title', 'location', 'description', or 'speaker'")
+    exit()
+
+# want to check for the events that have multiple speakers or description
+if col == 'speaker' or 'description':
+    for x in agenda.select():
+        if x[col] != None and val in x[col]:
+            print(x)
+            if x['session'] == 'Session':
+                checksub(x['id'])
+# every other event
+else:
+    info = agenda.select(where = {col:val})
+    for x in info:
+        print(x)
+        #checking for sub sessions
+        if x['session'] == 'Session':
+            checksub(x['id'])
+
 
 agenda.close()
